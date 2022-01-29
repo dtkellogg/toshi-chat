12e09a954faa20421f1f654d5cb86ad5f85c8f3b
Toshi <dtkellogg10@gmail.com>
2022-01-28 19:10:58 -0800

    removed compression from server

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,10 @@ const socketIo = require("socket.io");
 
 // middleware
 const cors = require("cors")
-const compression = require('compression')
 const { corsMiddleware } = require('./middleware/corsMiddleware')
 
 const app = express();
 app.use(express.json());
-app.use(compression({ threshold: 0 }));  // compress responses
 app.use(cors())  // CORS
 app.use(corsMiddleware);
 
